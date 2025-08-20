5c47594b26e6fac1b5cf2dcc73010679d16d4c19
HashAru <arunima0721@gmail.com>
2025-08-20 13:05:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,9 @@
 
 // Load environment variables
 require('dotenv').config();
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 
 const express = require('express');
 const mongoose = require('mongoose');
